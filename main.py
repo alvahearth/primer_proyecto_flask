2472c4ef0679038826dc1226a6a89154c6c35760
Raúl Coñumil <tekken5lord@gmail.com>
2022-11-03 21:44:54 -0300

    added passw and delete

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import email
 from email.policy import default
 from msilib import init_database
-from django.shortcuts import render
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from decouple import config
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
@@ -21,14 +21,14 @@ def create_db(app):
 
     #MySQL db
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/users'
-
-
     db = SQLAlchemy(app)
+    
         
     return db
     
 app = create_app()
 db = create_db(app)
+migrate = Migrate(app, db)
 
 @app.before_first_request
 def create_tables():
@@ -41,6 +41,19 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('La contraseña no es legible')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
 
@@ -49,6 +62,8 @@ class UserForm(FlaskForm):
     name = StringField("Nombre", validators=[DataRequired()]) 
     email = StringField('Email', validators=[DataRequired()])
     submit = SubmitField("Enviar")
+    password_hash = PasswordField('Contraseña', validators=[DataRequired(), EqualTo('password_hash2', message="Contraseñas iguales")])
+    password_hash2 = PasswordField('Repetir contraseña', validators=[DataRequired()])
     
 
 @app.route('/')
@@ -56,6 +71,7 @@ def index():
     first_name = "raul"
     favorites = ["pepperoni", "cheese", "pineapple"]
     all_users = Users.query.order_by(Users.date_added)
+    print(all_users)
     return render_template('index.html', 
                             first_name=first_name, 
                             favorites=favorites,
@@ -92,11 +108,18 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+            user = Users(name=form.name.data, 
+                            email=form.email.data, 
+                            password_hash=hashed_pw
+                            )
             db.session.add(user)
             db.session.commit()
         name = form.name.data
-    flash(f"Creaste el usuario { name }")
+        form.name.data = ''
+        form.email.data = ''
+        form.password_hash = ''
+        flash(f"Creaste el usuario { name }")
     
     our_users = Users.query.order_by(Users.date_added)
     return render_template('user_add.html', form=form, name=name, our_users=our_users)
@@ -124,6 +147,24 @@ def update(id):
                 form=form,
                 name_to_update=name_to_update
             )
+
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("El usuario se eliminó correctamente")
+        all_users = Users.query.order_by(Users.date_added)
+        return render_template('delete_user.html')
+    
+    except:
+        flash("Hubo un problema con la borración")
+        return render_template('user_add.html', form=form, name=name, our_users=all_users)
 
 
 @app.route('/spending')
